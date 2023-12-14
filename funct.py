492ea0d4c3b64ab91d0d891f769044c082bd2a7b
igerth <122569790+igerth@users.noreply.github.com>
2023-12-13 21:31:27 -0800

    took out the api key

--- funct.py
+++ funct.py
@@ -7,7 +7,7 @@ from werkzeug.utils import secure_filename
 
 app = Flask(__name__)
 
-openai.api_key = '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'
+openai.api_key = ' API KEY '
 
 overview = """Context for LIGN 101 - Introduction to the Study of Language at UCSD
 
@@ -380,7 +380,7 @@ def generate_flashcards(text, num_flashcards, model_name="gpt-4-1106-preview"):
     :return: List of flashcards in the specified format.
     """
 
-    openai.api_key = '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'  # Replace with your actual OpenAI API key
+    openai.api_key = ' API KEY '  # Replace with your actual OpenAI API key
 
     # Prepare the chat completion request
     messages = [
